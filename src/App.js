7617428e8fa428a05cac26f17b0e57a4f43b6f38
Mostafa <mostafarohanii0@gmail.com>
2022-06-02 19:33:11 +0430

    this is Crypto page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+// Components
+import Landing from './components/Landing';
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Coin.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// Style
+import styles from './Coin.module.css'
+
+const Coin = (props) => {
+
+    const { image, name, symbol, price, priceChange, marketCap } = props
+
+    return (
+        <div className={styles.container}>
+            <img className={styles.image} src={image} alt='icon' />
+            <span className={styles.name}>{name}</span>
+            <span className={styles.symbol}>{symbol.toUpperCase()}</span>
+            {/* Shows up to 2 decimal places */}
+            <span className={priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange}>{priceChange.toFixed(2)}</span>
+            <span className={styles.currentPrice}><span>Current Price:</span> ${price.toLocaleString()}</span>
+            {/* Separates the digits from all three digits */}
+            <span className={styles.marketCap}><span>Market Cap:</span> ${marketCap.toLocaleString()}</span>
+        </div>
+    );
+};
+
+export default Coin;
